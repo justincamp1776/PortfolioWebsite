98bfdb74cc783a6a0b9ee5ca1877e2a560889a98
Justin Campbell <justincamp1776@gmail.com>
2023-07-25 17:25:55 -0400

    modified project card

--- data.js
+++ data.js
@@ -79,24 +79,24 @@ let projects = [
     {
         "sectionId": 5,
         "title": "Order Entry EDI",
-        "description": "I consolidated 50 programs into a single application using factories, interfaces, abstractions, database redesign, and custom java packages. This program watches an FTP directory, parses ANSI X12, Excel, or flat files, validates/ scrubs data, writes orders as a tab-delimited messages, imports files into the new ERP platform, and communicates processing results to all relevant parties.",
-        "techStack": ".NET Windows Application OOP, Java Spring Data Service DAO Pattern, MSMS, SVN",
+        "description": "Consolidated 50 programs into a single application using OOP. This program utilizes file watcher on an FTP directory and parses order entries of various formats. Once each file is validated, customer part numbers are mapped to vendor item numbers. Order header and lines are dynamically built after each item is checked for availability within the customers market area. After orders complete processing and email is dynamically constructed and sent to all relavent parties.",
+        "techStack": ".NET Windows Application, Java Spring Data Service, SQL Server, SVN",
         "img": "motherboard.png",
         "repository":"EDIOrderEntry"
     },
     {
         "sectionId": 5,
         "title": "XML Invoicing",
-        "description": "Conducted thorough code analysis and meetings with stakeholders to define user requirements, mapped existing fields to prophet 21’s  standardized database schema, wrote compact and extensible  code, met with customers to complete acceptance testing and released.",
-        "techStack": ".NET, Java Spring MicroService, SSMS, SVN",
+        "description": "Conducted thorough code analysis and meetings with stakeholders to define user requirements, mapped existing fields to the ERP\'s (Prophet 21)  standardized database schema, wrote compact and extensible  code, met with customers to complete acceptance testing.",
+        "techStack": ".NET, Java Spring MicroService, SQL Server, SVN",
         "img": "invoice.png",
         "repository":""
     },
     {
         "sectionId": 5,
         "title": "Cloud Resume Challenge",
-        "description": "This site is driven by a serverless architecture that is powered by AWS. IT is CI/CD using AWS Code Pipeline and GitHub. The backend consists of a simple and efficient HTTP API, NO-SQL DynamoDB, and a lambda function. All documents are stored as objects within an S3 Bucket and deployed as a static website using CloudFront.",
-        "techStack": "html5, css, bootstrap, jQuery, AWS Services",
+        "description": "This site is driven by a serverless architecture that is powered by AWS. It is CI/CD using AWS Code Pipeline and GitHub. The backend consists of a simple and efficient HTTP API, NO-SQL DynamoDB, and a lambda function. All documents are stored as objects within an S3 Bucket and deployed as a static website using CloudFront.",
+        "techStack": "html5, css, bootstrap, jQuery, Github, AWS Services",
         "img": "cloudresume.png",
         "repository":""
     }
