a94e566d054e279cc897f13767857c0d100c43a7
Justin Campbell <justincamp1776@gmail.com>
2023-08-09 15:03:52 -0400

    added form validation + bootbox confirm + bootbox result alert

--- data.js
+++ data.js
@@ -11,16 +11,19 @@ let header = [
 let experience = [
     {
         "sectionId": 1,
-        "jobTitle": "Programmer",
+        "jobTitle": "Software Developer",
         "company" : "R.E. Michel Company",
         "imgPath": "images/product.png",
         "location" : "Glen Burnie, MD",
         "startDate" : "March 2022",
         "endDate" : "March 2023",
-        "d1" : "Integrated over 50 stand-alone EDI order-entry programs into a single .NET Windows Application using object oriented design patterns, refactored legacy software applications that serve a base of over a million customers and created an XML invoice messaging program in order to meet new system requirements, enhance efficiency, and improve maintainability.", 
-        "d2" : "Conducted extensive code analysis as well as trouble shooting sessions, with executives and stakeholders, to define user requirements and complete acceptance testing.",
-        "d3" : "Developed data service packages within an existing microservice architecture, using the DAO design pattern, while leveraging existing code bases for maintainability, readability, and future expansion.",
-        "d4" : "Restructured databases in Microsoft SQLServer Management Studio and assisted in the conversion of millions of records while following modern SQL standards to remit data redundancy, capitalize on entity relationships, and improve readability."
+        "d1" : "Full Stack Enterprise Application Development in C# and Java", 
+        "d2" : "Built EDI 850 Order Entry and EDI 810 Invoicing programs in C# that process over $1,000,000 annually.",
+        "d3" : "Created microservice processes in Java to support all Http requests for EDI accross 350 locations in 31 states.",
+        "d4" : "Refactored legacy sites written in Classic ASP and VB6 to support the integration with a new ERP.",
+        "d5" : "Aided in the migration of over 50 million records that were contained within our company’s 2000 plus data tables.",
+        "d6" : "Restructured SQL databases to implement modern standards, remit redundancy, and capitalize on entity-relationships.",
+        "stack" : ".NET, Spring, SSMS, JavaScript, JQuery, HTML5, CSS, Bootstrap, SVN"
     },
     {
         "sectionId": 1,
@@ -30,10 +33,13 @@ let experience = [
         "location" : "Columbia, MD",
         "startDate" : "Oct 2021",
         "endDate" : "March 2022",
-        "d1" : "Participated in meetings with senior engineers to determine user requirements and attended daily stand ups in an agile work environment.", 
-        "d2" : "Wrote eloquent , compact code for UI components, exhibiting low-latency,  in React using material UI and bootstrap.",
-        "d3" : "Designed and built a UI component in React for viewing, scheduling, and editing resource reservations.",
-        "d4" : "Manually debugged and tested units of code to confirm the continuity of contracts and ensure evaluations were accurate and dependable."
+        "d1" : "Full Stack Web Application Development in Python, JSX, and JavaScript using React and Django REST Framework.", 
+        "d2" : "Attended daily stand ups and met weekly sprint objectives in an agile work environment.",
+        "d3" : "Refactored user authentication processes in React.js applications.",
+        "d4" : "Created models and view functions in Python to support the user login process.",
+        "d5" : "Regularly delivered new UI features, improving the user experience.",
+        "d6" : "Manually debugged and tested new units of code. Also conducted manual regression testing to confirm all changes.",
+        "stack" : "React.js, Django REST, SQLite, Material UI, Redux, Jira, Bit Bucket"
     }
 
 ];
@@ -53,7 +59,7 @@ let about = [
     {
         "sectionId": 3,
         "greeting": 'Welcome to my cloud resume website! You are visitor #: ', 
-        "aboutPro":'In 2021, I pivoted from strength coach to software developer, or as I sometimes say, "Strength coach to strongly typed languages!". Over the past few years, I have worked an unprecedented amount of hours building honing a trade craft. Most recently, I had the priveledge of serving as a programmer in support of one of our nation\'s largest HVAC distributors. I have worked with many languages and within many frameworks. While there is still more to learn, I am confident that I will be an asset to your team.',
+        "aboutPro":'My aim is to reinforce my foundation in Software Engineering by serving within an organization where I can apply my current knowledge and skill set while also working to obtain a degree in Computer Science. I have built enterprise level applications and possess the problem solving skills to work through complex issues. I am hard working, analytical, and will add value to your team.',
         "aboutPriv":'When I am not working or studying I often roll around on mats practicing jiu-jitsu or pretending to be a rock star on my guitar. If you\'re reading this then I suppose you\'d like to learn about my development experience...I\'ll leave you to it!'
     },
 ];
@@ -79,15 +85,15 @@ let projects = [
     {
         "sectionId": 5,
         "title": "Order Entry EDI",
-        "description": "Consolidated 50 programs into a single application using OOP. This program utilizes file watcher on an FTP directory and parses order entries of various formats. Once each file is validated the customer part numbers are mapped to vendor item numbers. Order headers and lines are dynamically built after each item is checked for availability within the customers market area. After orders complete processing an email is dynamically constructed and programmatically sent to all relevant parties.",
-        "techStack": ".NET Windows Application, Java Spring Data Service, SQL Server, SVN",
+        "description": "This C# program accounts for many variances across 50 corporations. A FileWatcher scans a directory every 15 minutes. If a new write is detected an event is raised. Once each file is validated the customer part numbers are mapped to vendor item numbers. Order headers and lines are dynamically built after each item is checked for availability within the customer’s market area. Complete orders are imported into an Epicor ERP. Lastly, a detailed order summary is dynamically constructed and emailed to all relevant parties.",
+        "techStack": ".NET, Spring, SSMS, SVN",
         "img": "motherboard.png",
         "repository":"EDIOrderEntry"
     },
     {
         "sectionId": 5,
         "title": "XML Invoicing",
-        "description": "Met with stakeholders to define user requirements. Conducted thorough analysis in order to map legacy fields to the new ERP\'s (Prophet 21) standardized database schema. I wrote compact and extensible  code, conducted manual unit tests and met with customers to complete acceptance testing.",
+        "description": "This C# application transforms ANSI X12 EDI 810 Invoices into XML messages. It parses batch loads by scanning segments, identifying delimiters, and mapping to appropriate fields. It programmatically FTP’s and/ or emails XML Invoices to respective customers. This program is automated via Task Scheduler on Windows Server 2017.",
         "techStack": ".NET, Java Spring MicroService, SQL Server, SVN",
         "img": "invoice.png",
         "repository":""
@@ -95,8 +101,8 @@ let projects = [
     {
         "sectionId": 5,
         "title": "Cloud Resume Challenge",
-        "description": "This site is driven by a serverless architecture that is powered by AWS. I've had a lot of fun building this site. I drew outside the lines a bit by breaking away from the Z-pattern for larger screens; however, the mobile responsiveness should give users a clean feel. This site is CI/CD using AWS Code Pipeline and GitHub. The backend consists of a simple http API, dynamoDB(no-sql), and a lambda function. All documents are stored as objects within an S3 Bucket and deployed as a static website using CloudFront.",
-        "techStack": "html5, css, bootstrap, jQuery, Github, AWS Services",
+        "description": "This site is driven by a serverless architecture that is powered by AWS. This site is CI/CD using AWS CodePipeline and GitHub. The backend consists of an HTTP API, DynamoDB(No-CQL), and a Lambda Function. All documents are stored as objects within an S3 Bucket and deployed as a static website using CloudFront.",
+        "techStack": "avaScript, JQuery, HTML5,CSS, AWS HTTP API, DynamoDB, Lambda, AWS CloudFront, GitHub, CodePipeline",
         "img": "cloudresume.png",
         "repository":""
     }
